02dc7ebe892703af76bfe95f438b0d5a8cb58c13
AryaaGopal <arya.gopal@aspiresys.com>
2025-04-08 14:13:34 +0530

    Added Tile Link

--- components/molecules/tiles/recepie-tiles.component.tsx
+++ components/molecules/tiles/recepie-tiles.component.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { RecipeTileProps } from './recepie-tiles.types';
+import Link from 'next/link';
 
 
 
 const RecipeTile: React.FC<RecipeTileProps> = ({ title, imageUrl, cuisine, mealType }) => {
   return (
-    <div className="bg-white shadow rounded-lg overflow-hidden">
+ <Link href="/"> <div className="bg-white shadow rounded-lg overflow-hidden">
       <img src={imageUrl} alt={title} className="w-full h-40 object-cover" />
       <div className="p-4">
         <h3 className="font-semibold text-lg">{title}</h3>
         <p className="text-sm text-gray-500">{cuisine} • {mealType}</p>
       </div>
     </div>
+    </Link>
   );
 };
 
