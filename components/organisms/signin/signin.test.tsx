27b6cf04820c0daf0b7a07c7f22eaa7efbcb63b7
AryaaGopal <arya.gopal@aspiresys.com>
2025-04-16 15:38:25 +0530

    added testcases,test setup

--- components/organisms/signin/signin.test.tsx
+++ components/organisms/signin/signin.test.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { useRouter } from 'next/navigation';
 import { act } from 'react';
 import SignIn from './signin-component';
